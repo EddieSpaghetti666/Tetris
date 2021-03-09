b6a9e44359a23bfc561090eac18e651e92aeee8a
EddieSpaghetti666 <Dsmith141996@gmail.com>
2021-03-09 10:48:23 -0500

    Revert "fml"
    This reverts commit 7d313d3b58a023427b1b7585cdc28481ade72fdb.

--- Tetris/Tetris.cpp
+++ Tetris/Tetris.cpp
@@ -8,6 +8,12 @@
 
 int main() {
 
+<<<<<<< HEAD
+    srand((unsigned)time(NULL));
+=======
+    //Test
+>>>>>>> parent of b94f1fe (test)
+
     struct timeb frameStart, frameEnd;
     ftime(&frameStart);
     int frameTimeDiff;
